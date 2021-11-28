6c8a91543706b2930add477e4a4136565a195370
Riley Burke <rileyburke0@hotmail.com>
2021-11-28 11:08:18 -0330

    Create Blackjack.py

---
+++ Blackjack.py
@@ -0,0 +1,42 @@
+import random
+
+def shuffleDeck():
+    pass
+
+def loadDealerStatistics():
+    pass
+
+def saveDealerStatistics():
+    pass
+
+def bet():
+    pass
+
+def deckOfCards():
+    hearts = [["Hearts", "2"], ["Hearts" , "3"], ["Hearts", "4"], ["Hearts", "5"], ["Hearts", "6"], ["Hearts" , "7"], ["Hearts", "8"], ["Hearts", "9"],
+            ["Hearts", "10"], ["Hearts" , "Jack"], ["Hearts", "Queen"], ["Hearts", "King"], ["Hearts", "Ace"]]
+    diamonds = [["Diamonds", "2"], ["Diamonds" , "3"], ["Diamonds", "4"], ["Diamonds", "5"], ["Diamonds", "6"], ["Diamonds" , "7"], ["Diamonds", "8"], ["Diamonds", "9"],
+            ["Diamonds", "10"], ["Diamonds" , "Jack"], ["Diamonds", "Queen"], ["Diamonds", "King"], ["Diamonds", "Ace"]]
+    clubs = [["Clubs", "2"], ["Clubs" , "3"], ["Clubs", "4"], ["Clubs", "5"], ["Clubs", "6"], ["Clubs" , "7"], ["Clubs", "8"], ["Clubs", "9"],
+            ["Clubs", "10"], ["Clubs" , "Jack"], ["Clubs", "Queen"], ["Clubs", "King"], ["Clubs", "Ace"]]
+    spades = [["Spades", "2"], ["Spades" , "3"], ["Spades", "4"], ["Spades", "5"], ["Spades", "6"], ["Spades" , "7"], ["Spades", "8"], ["Spades", "9"],
+            ["Spades", "10"], ["Spades" , "Jack"], ["Spades", "Queen"], ["Spades", "King"], ["Spades", "Ace"]]
+    deck = hearts + diamonds + clubs + spades
+    return deck
+    
+def hitMe():
+    pass
+
+def play():
+    pass
+
+def mainMenu():
+    pass
+
+def main():
+    deck = deckOfCards()
+    print(deck)         #Testing deck
+    print(len(deck))  
+
+if __name__ == "__main__":
+    main()
