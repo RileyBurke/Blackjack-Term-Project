d6e0f97a5e93e5b9d60e3d111fefadf7c527fff4
Riley Burke <rileyburke0@hotmail.com>
2021-12-06 18:26:21 -0330

    Finishing touches
    Divided out some modules. Added some comments, changed a few variable names to be more descriptive.

--- Blackjack.py
+++ Blackjack.py
@@ -1,49 +1,20 @@
 import random
 import sys
+import Deck
+import DealerStatistics
+import Bank
 
-def getCardValue(drawnCard, total): #Returns the int value of a card drawn from the deck.
+def getCardValue(drawnCard, total): #Returns the number value of a card drawn from the deck.
     if drawnCard[1] == "2" or drawnCard[1] == "3" or drawnCard[1] == "4" or drawnCard[1] == "5" or drawnCard[1] == "6" or drawnCard[1] == "7" or drawnCard[1] == "8" or drawnCard[1] == "9":
         cardValue = int(drawnCard[1])
     elif drawnCard[1] == "10" or drawnCard[1] == "Jack" or drawnCard[1] == "Queen" or drawnCard[1] == "King":
         cardValue = 10
     elif drawnCard[1] == "Ace":
-        if total >= 11:
+        if total >= 11:       #Returns 11 for an Ace's value unless it would cause you to lose, in that case returns a 1.
             cardValue = 1
         else:
             cardValue = 11
     return cardValue
-
-def shuffleDeck(deck):   #Clears the deck, fills it, and shuffles it for the next game.
-    deck.clear()
-    deck = deckOfCards()
-    random.shuffle(deck)
-    return deck
-
-def loadDealerStatistics(): #Loads the dealer statistics file to a list to be appended to.
-    dealerStats = []
-    try:
-        with open ("dealerStats.txt") as file:
-            for row in file:
-                row = row.replace("\n","")
-                dealerStats.append(row)
-    except FileNotFoundError:
-        pass
-    except OSError:
-        print("File found - error reading the file - closing program")
-        sys.exit()
-    except Exception:
-        print("An unexpected exception occured - closing program")
-        sys.exit()
-    return dealerStats
-            
-def saveDealerStatistics(dealerStats): #Saves the amount the dealer makes to a text file after each game.
-    try:
-        with open("dealerStats.txt", "w") as file:
-            for stats in dealerStats:
-                file.write(str(stats) + "\n")
-    except Exception:
-        print("An unexpected exception occured - closing program")
-        sys.exit()
 
 def getBets(playerBanks, numberOfPlayers): #Accepts bets from each player.
     playerBets = [0] * numberOfPlayers
@@ -61,7 +32,7 @@ def getBets(playerBanks, numberOfPlayers): #Accepts bets from each player.
                         continue
                     else:
                         print("Not enough funds. You have $" + str(playerBanks[counter]))
-                        addFunds(playerBanks, counter+1)
+                        Bank.addFunds(playerBanks, counter+1)
                         continue
                 else:
                     playerBanks[counter] -= playerBets[counter]
@@ -73,97 +44,95 @@ def getBets(playerBanks, numberOfPlayers): #Accepts bets from each player.
     print()
     return playerBets
 
-
-def deckOfCards():    #Initializes deck of cards.
-    deck = [["Hearts", "2"], ["Hearts" , "3"], ["Hearts", "4"], ["Hearts", "5"], ["Hearts", "6"], ["Hearts" , "7"], ["Hearts", "8"], ["Hearts", "9"],
-            ["Hearts", "10"], ["Hearts" , "Jack"], ["Hearts", "Queen"], ["Hearts", "King"], ["Hearts", "Ace"], ["Diamonds", "2"], ["Diamonds" , "3"],
-            ["Diamonds", "4"], ["Diamonds", "5"], ["Diamonds", "6"], ["Diamonds" , "7"], ["Diamonds", "8"], ["Diamonds", "9"], ["Diamonds", "10"],
-            ["Diamonds" , "Jack"], ["Diamonds", "Queen"], ["Diamonds", "King"], ["Diamonds", "Ace"], ["Clubs", "2"], ["Clubs" , "3"], ["Clubs", "4"],
-            ["Clubs", "5"], ["Clubs", "6"], ["Clubs" , "7"], ["Clubs", "8"], ["Clubs", "9"], ["Clubs", "10"], ["Clubs" , "Jack"], ["Clubs", "Queen"],
-            ["Clubs", "King"], ["Clubs", "Ace"], ["Spades", "2"], ["Spades" , "3"], ["Spades", "4"], ["Spades", "5"], ["Spades", "6"], ["Spades" , "7"],
-            ["Spades", "8"], ["Spades", "9"], ["Spades", "10"], ["Spades" , "Jack"], ["Spades", "Queen"], ["Spades", "King"], ["Spades", "Ace"]]
-    return deck
+def payout(playerBets, playerBanks, playerTotals, counter): #Pays out winnings to winning players. Rewards 3:2 for blackjack and 1:1 otherwise.
+    if playerTotals == 21:
+        payout = round(playerBets * 2.5, 2)
+    else:
+        payout = round(playerBets * 2, 2)
+    playerBanks[counter] += payout
+    print("Bet: $" + str(playerBets) + "  Winnings: $" + str(payout) + "  New total: $" + str(playerBanks[counter]))
+    return payout
     
 def hitPlayer(deck, total, counter, turnNumber, flag): #Adds a card to the players hand.
     while True:
         if turnNumber == 1:
             player_card = random.choice(deck)
             print("Player " + str(counter + 1) + " has drawn a " + player_card[1] + " of " + player_card[0] + ".")
-            card = getCardValue(player_card, total)
+            cardValue = getCardValue(player_card, total)
             deck.remove(player_card)
-            total += card
+            total += cardValue
             return total
         else:
             player_card = random.choice(deck)
             print("Player " + str(counter + 1) + " has drawn a " + player_card[1] + " of " + player_card[0] + ".")
-            card = getCardValue(player_card, total)
+            cardValue = getCardValue(player_card, total)
             deck.remove(player_card)
-            total += card
+            total += cardValue
             if total == 21:
                 flag = False
             elif total > 21:
                 flag = False
             return total, flag
 
-def hitDealer(deck, dealerTotal, turnNumber, dealerFlag): #Adds a card to the dealers hand.
+def hitDealer(deck, dealerTotal, turnNumber, dealerActive): #Adds a card to the dealers hand.
     if turnNumber == 1:
         player_card = random.choice(deck)
         print("The dealer has drawn a " + player_card[1] + " of " + player_card[0] + ".")
         print()
-        card = getCardValue(player_card, dealerTotal)
+        cardValue = getCardValue(player_card, dealerTotal)
         deck.remove(player_card)
-        dealerTotal += card
+        dealerTotal += cardValue
         return dealerTotal
     else:
         while True:
-            if dealerTotal < 17:
+            if dealerTotal < 17: #Dealer must hit if total is less than 17.
                 player_card = random.choice(deck)
                 print("The dealer has drawn a " + player_card[1] + " of " + player_card[0] + ".")
-                card = getCardValue(player_card, dealerTotal)
+                cardValue = getCardValue(player_card, dealerTotal)
                 deck.remove(player_card)
-                dealerTotal += card
+                dealerTotal += cardValue
                 print("Total is now " + str(dealerTotal) + ".")
                 print()
-                return dealerTotal, dealerFlag
+                return dealerTotal, dealerActive
             elif dealerTotal == 21:
                 print("Dealer has blackjack.")
                 print()
-                dealerFlag = False
-                return dealerTotal, dealerFlag
+                dealerActive = False
+                return dealerTotal, dealerActive
             elif dealerTotal > 21:
                 print("Dealer has busted out.")
                 print()
-                dealerFlag = False
-                return dealerTotal, dealerFlag
+                dealerActive = False
+                return dealerTotal, dealerActive
             else:
                 print("Dealer stands with " + str(dealerTotal) + ".")
                 print()
-                dealerFlag = False
-                return dealerTotal, dealerFlag
+                dealerActive = False
+                return dealerTotal, dealerActive
 
 def playGame(deck, playerBanks, numberOfPlayers): #Main blackjack function.
-    dealerStats = loadDealerStatistics()
+    dealerStats = DealerStatistics.loadDealerStatistics()
     print()
     choice = "y"
     while choice.lower() == "y":
         playerTotals = [0] * numberOfPlayers
         playerFlags = [True] * numberOfPlayers
-        playerFlag = True
-        dealerFlag = True
+        playersActive = True
+        dealerActive = True
         dealerTotal = 0
         turnNumber = 1
         counter = 0
         playerBets = getBets(playerBanks, numberOfPlayers)
-        while playerFlag == True:
+        while playersActive == True:
             while counter < len(playerTotals): #Loops through each player for a given turn
-                if turnNumber == 2:     #Turn number 2
+                if turnNumber == 2:            #Turn number 2
                     playerTotals[counter], playerFlags[counter] = hitPlayer(deck, playerTotals[counter], counter, turnNumber, playerFlags[counter])
                     print("Total is now " + str(playerTotals[counter]) + ".")
                     if playerTotals[counter] == 21:
                         print("Player " + str(counter + 1) + " has blackjack!")
                     print()
                     counter += 1
-                elif turnNumber > 2:    #Turn >2, gives hit/stand options.
+                elif turnNumber > 2:           #Turn >2, gives hit/stand options.
                     if playerFlags[counter] == True:
                         gameOptions(deck, playerTotals, counter, turnNumber, playerBanks, playerBets, playerFlags)
                         if playerFlags[counter] == True:
@@ -183,19 +152,19 @@ def playGame(deck, playerBanks, numberOfPlayers): #Main blackjack function.
                         counter += 1
                 elif counter == len(playerTotals):
                     break
-                else:                   #Turn number 1
+                else:                #Turn number 1
                     playerTotals[counter] = hitPlayer(deck, playerTotals[counter], counter, turnNumber, playerFlags[counter])
                     print()
                     counter += 1
             if turnNumber == 1:
-                dealerTotal = hitDealer(deck, dealerTotal, turnNumber, dealerFlag)
+                dealerTotal = hitDealer(deck, dealerTotal, turnNumber, dealerActive)
             turnNumber += 1
             counter = 0
             if not any(playerFlags): #Checks for any True flags, if all flags are false the game will end.
-                playerFlag = False
+                playersActive = False
                 
-        while dealerFlag == True:
-            dealerTotal, dealerFlag = hitDealer(deck, dealerTotal, turnNumber, dealerFlag)
+        while dealerActive == True:
+            dealerTotal, dealerActive = hitDealer(deck, dealerTotal, turnNumber, dealerActive)
 
         payoutCounter = 0
         dealerWinnings = 0
@@ -223,8 +192,8 @@ def playGame(deck, playerBanks, numberOfPlayers): #Main blackjack function.
             print()
 
         dealerStats.append(dealerWinnings)
-        saveDealerStatistics(dealerStats)        
-        deck = shuffleDeck(deck)
+        DealerStatistics.saveDealerStatistics(dealerStats)        
+        deck = Deck.shuffleDeck(deck)
         choice = input("Play again? (y/n): ")
         print()
     mainMenu()
@@ -251,37 +220,7 @@ def gameOptions(deck, playerTotals, counter, turnNumber, playerBanks, playerBets
             break
         else:
             print("Invalid option.")
-            continue
-
-def checkBalance(playerBanks): #Allows players to check their balance from the main menu.
-    print()
-    while True:
-        if len(playerBanks) > 1:
-            try:
-                playerNumber = int(input("Enter number of player to check (1-" + str(len(playerBanks)) + "): "))
-            except ValueError:
-                print("Invalid player number. Try again.")
-                print()
-                continue
-        else:
-            playerNumber = 1
-        if playerNumber <= len(playerBanks) and playerNumber > 0:
-            print("You have $" + str(playerBanks[playerNumber - 1]) + " in funds.")
-            print()
-            break
-        else:
-            print("Invalid player number. Try again.")
-            print()
             continue       
-    
-def payout(playerBets, playerBanks, playerTotals, counter): #Pays out winnings to winning players. Rewards 3:2 for blackjack and 1:1 otherwise.
-    if playerTotals == 21:
-        payout = round(playerBets * 2.5, 2)
-    else:
-        payout = round(playerBets * 2, 2)
-    playerBanks[counter] += payout
-    print("Bet: $" + str(playerBets) + "  Winnings: $" + str(payout) + "  New total: $" + str(playerBanks[counter]))
-    return payout
         
 def mainMenu(): #Prints menu options.
     print("1. Play")
@@ -296,39 +235,6 @@ def greeting(): #Prints initial greeting.
     print("Blackjack")
     print()
 
-def addFunds(playerBanks, playerNumber):
-    #Adds funds to player banks. If chosen in menu it gives an option to choose player. If player runs out of money in game it chooses the player for you before you can bet.
-    print()
-    while True:
-        if len(playerBanks) > 1 and playerNumber == 0:
-            try:
-                playerNumber = int(input("Enter which player to add funds to (1-" + str(len(playerBanks)) + "): "))
-            except ValueError:
-                print("Invalid player number. Try again.")
-                print()
-                continue
-        elif playerNumber <= len(playerBanks) and playerNumber > 0 or len(playerBanks) == 1:
-            if len(playerBanks) == 1:
-                playerNumber = 1
-            try:
-                addedAmount = int(input("How much money would you like to add?: $"))
-                if addedAmount < 0:
-                    print("Invalid amount entered, try again.")
-                else:
-                    playerBanks[playerNumber - 1] += addedAmount
-                    print("Added $" + str(addedAmount) + " to your bank.")
-                    break
-            except ValueError:
-                print("Invalid integer amount entered.")
-                print()
-                break
-        else:
-            print("Invalid player number. Try again.")
-            print()
-            continue
-    print("You now have $" + str(playerBanks[playerNumber - 1]) + " in funds.")
-    print()
-
 def enterCommand(deck, playerBanks, numberOfPlayers): #Accepts commands to navigate through the menu.
     mainMenu()
     while True:
@@ -337,9 +243,9 @@ def enterCommand(deck, playerBanks, numberOfPlayers): #Accepts commands to navig
             if command == 1:
                 deck = playGame(deck, playerBanks, numberOfPlayers)
             elif command == 2:
-                checkBalance(playerBanks) 
+                Bank.checkBalance(playerBanks) 
             elif command == 3:
-                addFunds(playerBanks, 0)
+                Bank.addFunds(playerBanks, 0)
             elif command == 4:
                 break
             else:
@@ -364,27 +270,11 @@ def setNumberOfPlayers(): #Sets the number of players to play the game.
         except ValueError:
             print("Invalid number of players. Must be a number from 1 to 5.")
 
-def setPlayerBanks(numberOfPlayers): #Sets the initial values for the bank of each player.
-    playerBanks = [0] * numberOfPlayers
-    counter = 0
-    for player in playerBanks:
-        while True:
-            try:
-                playerBanks[counter] = int(input("How much money would you like to enter for player " + str(counter + 1) + "?: $"))
-                if playerBanks[counter] >= 5:
-                    counter += 1
-                    break
-                else:
-                    print("Not enough money to place a bet.")
-            except ValueError:
-                print("Invalid amount, please try again.")
-    return playerBanks
-
 def main():
-    deck = deckOfCards()
+    deck = Deck.deckOfCards()
     greeting()
     numberOfPlayers = setNumberOfPlayers()
-    playerBanks = setPlayerBanks(numberOfPlayers)
+    playerBanks = Bank.setPlayerBanks(numberOfPlayers)
     print()
     enterCommand(deck, playerBanks, numberOfPlayers)
     print("Bye!")
---
+++ DealerStatistics.py
@@ -0,0 +1,25 @@
+def loadDealerStatistics(): #Loads the dealer statistics file to a list to be appended to.
+    dealerStats = []
+    try:
+        with open ("dealerStats.txt") as file:
+            for row in file:
+                row = row.replace("\n","")
+                dealerStats.append(row)
+    except FileNotFoundError:
+        pass
+    except OSError:
+        print("File found - error reading the file - closing program")
+        sys.exit()
+    except Exception:
+        print("An unexpected exception occured - closing program")
+        sys.exit()
+    return dealerStats
+
+def saveDealerStatistics(dealerStats): #Saves the amount the dealer makes to a text file after each game.
+    try:
+        with open("dealerStats.txt", "w") as file:
+            for stats in dealerStats:
+                file.write(str(stats) + "\n")
+    except Exception:
+        print("An unexpected exception occured - closing program")
+        sys.exit()
